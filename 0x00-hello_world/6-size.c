16530c534fca71b53b79f94a8649ad04776c4fee
ruthdube <chiporuth@gmail.com>
2023-02-23 02:36:56 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
  *Size of a long long int: 8 byte(s)
  *Size of a float: 4 byte(s)
 */ 
-printf("char size = %zu", sizeof (char));
-printf("\nint size = %zu", sizeof (int));
-pringf("\nlong int size = %zu", sizeof (long int));
-printf("\nlong long int size =%zu", sizeof (long long int));
-printf("\nfloat size = %zu", sizof (float));
+printf("char size = %zu", sizeof(char));
+printf("\nint size = %zu", sizeof(int));
+pringf("\nlong int size = %zu", sizeof(long int));
+printf("\nlong long int size =%zu", sizeof(long long int));
+printf("\nfloat size = %zu", sizeof(float));
 return (0);
 }
